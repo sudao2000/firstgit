5970475ee056a6f765a05d72a5e8e378546ada1d
sudao2000 <sudao2000@126.com>
2014-11-23 21:35:12 +0800

    add product list

---
+++ src/com/art/tech/ProductListActivity.java
@@ -0,0 +1,11 @@
+package com.art.tech;
+
+import android.os.Bundle;
+
+public class ProductListActivity extends BaseActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.product_list_view);
+	}
+}
